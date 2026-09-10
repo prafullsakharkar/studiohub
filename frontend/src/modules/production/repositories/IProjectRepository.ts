d1bbd7a7a71582223989e904917592c8d32740e2
Prafull Sakharkar <prafull.sakharkar@intra.madassemblage.com>
2026-09-10 09:53:28 +0530

    chore: remove frontend directory

--- frontend/src/modules/production/repositories/IProjectRepository.ts
+++
@@ -1,6 +0,0 @@
-import { IBaseRepository } from '@/core/repository/IBaseRepository';
-import { Project } from '@/types/projects';
-
-export interface IProjectRepository extends IBaseRepository<Project, Partial<Project>, Partial<Project>> {
-  getStatistics?(projectId: string): Promise<Record<string, number>>;
-}
--- frontend/src/modules/tasks/repositories/ITaskRepository.ts
+++
@@ -1,4 +0,0 @@
-import { IBaseRepository } from '@/core/repository/IBaseRepository';
-import { Task } from '@/types/tasks';
-
-export type ITaskRepository = IBaseRepository<Task, Partial<Task>, Partial<Task>>;
